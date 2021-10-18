af7501a1126199165bff4a822fe5e9b80d869937
soushi <sosimds@gmail.com>
2021-10-18 15:25:07 +0900

    データベース構造を変更。日時記録を created, updated, at の三種類で行うように変更。

--- edit.py
+++ edit.py
@@ -45,7 +45,7 @@ class NikkiListbox(tk.Listbox):
         self.load_nikki()
 
     def _conv_nikki_to_text(self, nikki):
-        return '{} {} [{}] {}'.format(nikki[0], nikki[1], nikki[3], nikki[2][:20])
+        return '{n[id]} {n[at]} [{n[groupe]}] {n[comment]}'.format(n=nikki)
 
     def load_nikki(self, limit=100):
         self._logger.debug('load nikki')
@@ -64,7 +64,7 @@ class NikkiListbox(tk.Listbox):
             self._delete_nikki(index)
 
     def _delete_nikki(self, index):
-        delete_nikki_by_id(self._nikki_list[index][0])
+        delete_nikki_by_id(self._nikki_list[index]['id'])
         self._nikki_list.pop(index)
         self.delete(index)
 
@@ -84,7 +84,7 @@ class NikkiListbox(tk.Listbox):
         self.reload_nikki(index)
 
     def reload_nikki(self, index):
-        nikki = load_nikki_from_db_by_id(self._nikki_list[index][0])
+        nikki = load_nikki_from_db_by_id(self._nikki_list[index]['id'])
         text = self._conv_nikki_to_text(nikki)
         self._nikki_list[index] = nikki
         self.delete(index)
@@ -108,19 +108,25 @@ class Editor(tk.Toplevel):
 
         self.grab_set()
         
-        var_id = tk.StringVar(value=nikki[0])
-        var_created = tk.StringVar(value=nikki[1])
-        var_groupe = tk.StringVar(value=nikki[3])
+        var_id = tk.StringVar(value=nikki['id'])
+        var_created = tk.StringVar(value=nikki['created'])
+        var_updated = tk.StringVar(value=nikki['updated'])
+        var_at = tk.StringVar(value=nikki['at'])
+        var_groupe = tk.StringVar(value=nikki['groupe'])
 
         label_id = tk.Label(self, text='id')
         label_created = tk.Label(self, text='created')
+        label_updated = tk.Label(self, text='updated')
+        label_at = tk.Label(self, text='at')
         label_comment = tk.Label(self, text='comment')
         label_groupe = tk.Label(self, text='groupe')
 
         entry_id = tk.Entry(self, textvariable=var_id, state='readonly')
-        frame_datetime = DatetimeFrame(self, var=var_created)
+        entry_created = tk.Entry(self, textvariable=var_created, state='readonly')
+        entry_updated = tk.Entry(self, textvariable=var_updated, state='readonly')
+        frame_datetime = DatetimeFrame(self, var=var_at)
         text_comment = tk.Text(self)
-        text_comment.insert('1.0', nikki[2])
+        text_comment.insert('1.0', nikki['comment'])
         entry_groupe = tk.Entry(self, textvariable=var_groupe)
 
         ctrl_frame = tk.Frame(self)
@@ -132,28 +138,32 @@ class Editor(tk.Toplevel):
 
         label_id.grid(column=0, row=0)
         label_created.grid(column=0, row=1)
-        label_comment.grid(column=0, row=2)
-        label_groupe.grid(column=0, row=3)
+        label_updated.grid(column=0, row=2)
+        label_at.grid(column=0, row=3)
+        label_comment.grid(column=0, row=4)
+        label_groupe.grid(column=0, row=5)
         entry_id.grid(column=1, row=0)
-        frame_datetime.grid(column=1, row=1)
-        text_comment.grid(column=1, row=2)
-        entry_groupe.grid(column=1, row=3)
-        ctrl_frame.grid(column=0, row=4, columnspan=2)
+        entry_created.grid(column=1, row=1)
+        entry_updated.grid(column=1, row=2)
+        frame_datetime.grid(column=1, row=3)
+        text_comment.grid(column=1, row=4)
+        entry_groupe.grid(column=1, row=5)
+        ctrl_frame.grid(column=0, row=6, columnspan=2)
 
         self._var_id = var_id
-        self._var_created = var_created
+        self._var_at = var_at
         self._text_comment = text_comment
         self._var_groupe = var_groupe
         
     def update(self):
         self._logger.debug('updating nikki')
         nikki_id = self._var_id.get()
-        created = self._var_created.get() 
+        at = self._var_at.get() 
         comment = self._text_comment.get('1.0', 'end-1c')
         groupe = self._var_groupe.get()
         if groupe == '':
             groupe = None
-        update_nikki(nikki_id, created, comment, groupe)
+        update_nikki(nikki_id, at, comment, groupe)
         self.destroy()
 
     def cancel(self):
@@ -172,13 +182,13 @@ class DatetimeFrame(tk.Frame):
         frame_top = tk.Frame(self)
         button_minus7Days = tk.Button(frame_top, text='-7日', command=self.make_add_func(timedelta(days=-7)))
         button_minus1Day = tk.Button(frame_top, text='-1日', command=self.make_add_func(timedelta(days=-1)))
-        entry_created = tk.Entry(frame_top, textvariable=var)
+        entry_at = tk.Entry(frame_top, textvariable=var)
         button_plus1Day = tk.Button(frame_top, text='+1日', command=self.make_add_func(timedelta(days=1)))
         button_plus7Days = tk.Button(frame_top, text='+7日', command=self.make_add_func(timedelta(days=7)))
 
         button_minus7Days.grid(column=0, row=0)
         button_minus1Day.grid(column=1, row=0)
-        entry_created.grid(column=2, row=0, padx=8, pady=8)
+        entry_at.grid(column=2, row=0, padx=8, pady=8)
         button_plus1Day.grid(column=3, row=0)
         button_plus7Days.grid(column=4, row=0)
 
@@ -238,16 +248,22 @@ class DatetimeFrame(tk.Frame):
         self._var.set(self._default_var)
 
 
+#データベースに接続する
 def get_db():
-    return sqlite3.connect(DATABASE_PATH)
+    """connect to db"""
+    logger.debug('connecting to db')
+    db = sqlite3.connect(DATABASE_PATH)
+    db.row_factory = sqlite3.Row
+    return db
 
+#データベースから指定した件数読み込む。
 def load_nikki_from_db(limit=100, order='DESC'):
     """read nikki from database"""
     logger.debug('read nikki from database')
     db = None
     try:
         db = get_db()
-        nikki_list = db.execute("SELECT id, DATETIME(created, 'localtime'), comment, groupe FROM nikki ORDER BY id DESC LIMIT ?", (limit,)).fetchall()
+        nikki_list = db.execute("SELECT id, DATETIME(created, 'localtime') AS created, DATETIME(updated, 'localtime') AS updated, DATETIME(at, 'localtime') AS at, comment, groupe FROM nikki ORDER BY id DESC LIMIT ?", (limit,)).fetchall()
     except sqlite3.OperationalError:
         logger.error('failed to read nikki from database')
         raise
@@ -262,7 +278,7 @@ def load_nikki_from_db_by_id(nikki_id):
     db = None
     try:
         db = get_db()
-        nikki_list = db.execute("SELECT id, DATETIME(created, 'localtime'), comment, groupe FROM nikki WHERE id = ?", (nikki_id,)).fetchall()
+        nikki_list = db.execute("SELECT id, DATETIME(created, 'localtime') AS created, DATETIME(updated, 'localtime') AS updated, DATETIME(at, 'localtime') AS at, comment, groupe FROM nikki WHERE id = ?", (nikki_id,)).fetchall()
         if nikki_list == []:
             logger.warning('failed to load nikki : no such nikki No.{}'.format(nikki_id))
             nikki = []
@@ -307,13 +323,13 @@ def delete_nikki_by_id(nikki_id):
         if db:
             db.close()
 
-def update_nikki(nikki_id, created, comment, groupe):
+def update_nikki(nikki_id, at, comment, groupe):
     """update nikki"""
     logger.debug('updating nikki No.{}'.format(nikki_id))
     db = None
     try:
         db = get_db()
-        db.execute("UPDATE nikki SET created = DATETIME(?, 'utc'), comment = ?, groupe = ? WHERE id = ?", (created, comment, groupe, nikki_id))
+        db.execute("UPDATE nikki SET updated = CURRENT_TIMESTAMP, at = DATETIME(?, 'utc'), comment = ?, groupe = ? WHERE id = ?", (at, comment, groupe, nikki_id))
         db.commit()
         logger.debug('updating nikki is complete')
     except sqlite3.OperationalError:
